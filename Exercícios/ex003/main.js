5c65276d38934763c6db1a0c5fc2d3906a6dd159
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-11-02 12:47:26 -0300

    Exercícios

---
+++ Exercícios/ex003/main.js
@@ -0,0 +1,17 @@
+for(let i = 0; i<=10; i++)
+{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+}
+
+//--------------------------
+
+let j = 10;
+
+while(j>0)
+{
+    console.log(j);
+    j--;
+}
