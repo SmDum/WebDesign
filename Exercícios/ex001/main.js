94867878acfcb178e1e4a2075ada887a6370040e
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-11-02 11:55:41 -0300

    Exercícios

---
+++ Exercícios/ex001/main.js
@@ -0,0 +1,20 @@
+let idade = 25;
+
+console.log(idade);
+console.log(typeof(idade));
+
+//--------------------------
+
+function media(num1, num2)
+{
+    return (num1 + num2)/2;
+}
+
+console.log(media(10, 20));
+
+//--------------------------
+
+let texto = "5";
+let numero = parseInt(texto);
+
+console.log(numero);
