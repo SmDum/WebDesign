1b38ff0f3d63e002ca24613cac3873759db9844e
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-11-02 12:54:56 -0300

    Exercícios

---
+++ Exercícios/ex004/main.js
@@ -0,0 +1,14 @@
+const area = (largura, comprimento) => {
+    return largura * comprimento;
+}
+
+console.log(area(10, 20));
+
+//--------------------------
+
+const soma = function(a, b, c)
+{
+    return a+b+c;
+}
+
+console.log(soma(10,20,30));
