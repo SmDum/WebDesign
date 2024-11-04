afd7398e15a7bfa3dfa6f8343afc9443bbd36333
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-11-04 17:11:31 -0300

    Exercícios

--- Exercícios/ex015/script.js
+++ Exercícios/ex015/script.js
@@ -14,3 +14,16 @@ function mudarImagem() {
     const troca = img.getAttribute("src") === "images/1.webp" ? "images/2.webp" : "images/1.webp";
     img.setAttribute("src", troca);
 }
+
+function mudarConteudo() {
+    let textoAlterado = false;
+
+    const conteudo = document.getElementById("titulo");
+    const trocatitulo = conteudo.innerHTML === "Texto Original" ? "Texto Alternativo" : "Texto Original";
+    conteudo.innerHTML = trocatitulo;
+}
+
+function mudarClasse() {
+    const classe = document.getElementById("texto4");
+    classe.classList.toggle("mudar");
+}
