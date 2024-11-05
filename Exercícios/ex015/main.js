daf1192a0f28ee43df3feaac83e3f7aacf66c6c4
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-11-05 15:57:00 -0300

    Exercícios

--- Exercícios/ex015/main.js
+++ Exercícios/ex015/main.js
@@ -1,5 +1,24 @@
-function mudarCor() {}
-function mudarImagem() { }
-function mudarConteudo() {}
-function mudarClasse() {}
-function audio() {}
+function mudarCor() {
+    const texto = document.getElementById("texto");
+    texto.style.color = texto.style.color === "black" ? "red" : "black";
+}
+
+function mudarImagem() {
+    const img = document.getElementById("minhaImagem");
+    img.src = img.src.endsWith("images/1.webp") ? "images/2.webp" : "images/1.webp"
+}
+
+function mudarConteudo() {
+    const conteudo = document.getElementById("titulooficial");
+    conteudo.innerText = conteudo.innerText === "Título Original" ? "Título Alternativo" : "Título Original";
+}
+
+function mudarClasse() {
+    const classe = document.getElementById("texto4");
+    classe.classList.toggle("mudar");
+}
+
+function audio() {
+    const audio = document.getElementById("audio");
+    audio.play();
+}
