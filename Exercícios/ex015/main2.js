fd1b51dc3367b68edc7182085190f9bc3855ce60
SmDum <samueldemoraesdelgado2005@gmail.com>
2024-11-05 16:13:54 -0300

    Exercícios

---
+++ Exercícios/ex015/main2.js
@@ -0,0 +1,24 @@
+function mudarCor() {
+    const conteudo = document.getElementById("texto");
+    conteudo.style.color = conteudo.style.color === "black" ? "red" : "black";
+}
+
+function mudarImagem() {
+    const img = document.getElementById("minhaImagem");
+    img.src = img.src.endsWith("images/1.webp") ? "images/2.webp" : "images/1.webp";
+}
+
+function mudarConteudo() {
+    const texto = document.getElementById("titulooficial");
+    texto.innerText = texto.innerText === "Título Original" ? "Título Alternativo" : "Título Original";
+}
+
+function mudarClasse() {
+    const classe = document.getElementById("texto4");
+    classe.classList.toggle("mudar");
+}
+
+function audio() {
+    const audio = document.getElementById("audio");
+    audio.play();
+}
